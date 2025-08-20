39fef577f4818ce9c0508bebc9398fdfeb4cd197
maltiez2 <maltiez2@gmail.com>
2025-08-21 01:49:54 +0300

    wip

--- source/ModSystems.cs
+++ source/ModSystems.cs
@@ -4,5 +4,8 @@ namespace FueledWearableLights;
 
 public sealed class FueledWearableLightsSystem : ModSystem
 {
-
+    public override void Start(ICoreAPI api)
+    {
+        api.RegisterItemClass("FueledWearableLights:WearableFueledLightSource", typeof(WearableFueledLightSource));
+    }
 }
